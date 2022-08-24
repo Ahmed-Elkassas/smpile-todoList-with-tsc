7f667f395f0cf18837523189a7736a336e8477cf
Ahmed-Basuony <ahmed.basouny@awstreams.com>
2022-08-24 10:38:29 +0200

    Create simple todo-list using snowpack build tool and tailwind for beautiful css

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,65 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import {v4 as uuidv4} from 'uuid';
 
-import confetti from 'canvas-confetti';
+type Task  = {
+  id: string,
+  title: string,
+  complated: boolean,
+  createdAt: Date,
+}
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+// console.log(uuidv4())
+
+const list = document.getElementById("list") as HTMLUListElement;
+const form = document.querySelector<HTMLFormElement>('form');
+const input = document.querySelector<HTMLInputElement>('input');
+
+const tasks: Task[] = loadedTasks();
+
+tasks.forEach(addNewTask)
+
+form?.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if(input?.value == "" || input?.value == null) return;
+
+  const newTask: Task = {
+    id: uuidv4(),
+    title: input.value,
+    complated: true,
+    createdAt: new Date()
+  }
+  
+  saveTasks()
+
+  tasks.push(newTask);
+
+  addNewTask(newTask)
+  input.value = ""
+})
+
+
+function addNewTask(task: Task) {
+  const itemList = document.createElement('li');
+  const label = document.createElement('label');
+  const checkbox = document.createElement('input');
+  checkbox.addEventListener("change", () => {
+    task.complated = checkbox.checked;
+    // console.log(task)
+    saveTasks()
+  })
+  checkbox.type = 'checkbox';
+  checkbox.checked = task.complated;
+  label.append(checkbox, task.title);
+  itemList.append(label);
+  list?.append(itemList);
+}
+
+function saveTasks() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function loadedTasks(): Task[] {
+  const tasksJSON  = localStorage.getItem("tasks");
+
+  if(tasksJSON === null) return [];
+ return JSON.parse(tasksJSON);
+}
